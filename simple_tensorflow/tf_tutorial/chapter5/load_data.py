1eb9bea075c7f86d78f9c8d798e991416c77769e
sean.jie <sean.jie>
2018-12-10 10:46:57 +0800

    add chapter5

---
+++ simple_tensorflow/tf_tutorial/chapter5/load_data.py
@@ -0,0 +1,14 @@
+# from tensorflow.examples.tutorials.mnist import input_data
+#
+# mnist = input_data.read_data_sets("/Users/sean.jie/my_codes/general_ml/simple_tensorflow/tf_tutorial/chapter5/MNIST_data", one_hot = True)
+# print("Training data size :", mnist.train.num_examples)
+# print("Validating data size :", mnist.validation.num_examples)
+# print("Testing data size :", mnist.test.num_examples)
+# print("Example data :", mnist.train.images[0])
+# print("Training data label:", mnist.train.images[0])
+
+a = '''
+寒蝉凄切，对长亭晚，骤雨初歇。都门帐饮无绪，留恋处，兰舟催发。执手相看泪眼，竟无语凝
+噎。念去去，千里烟波，暮霭沉沉楚天阔。
+多情自古伤离别，更那堪冷落清秋节！今宵酒醒何处？杨柳岸，晓风残月。此去经年⑩，应是良辰好景虚设。便纵有千种风情⑪，更与何人说'''
+print(a)
